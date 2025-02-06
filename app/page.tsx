49796881ab5d9228b5f741c3f9533faca38080cd
Jeseok Lee <jaesek1223@icloud.com>
2025-02-06 13:05:49 +0900

    profile pic added

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,36 @@
+import Image from "next/image";
+
 // src/app/page.tsx
 export default function Home() {
 	return (
-		<div className="prose prose-invert max-w-none">
-			<h1>Welcome to my Developer Blog</h1>
-			<h2>Who am I?</h2>
-			<p>
-				Hello there 👋
-				<br />
-				I&rsquo;m <b>Jeseok Lee</b> from south Korea
-			</p>
-			<p>
-				This blog is a demo version. Please wait a day, and you&rsquo;ll have
-				access to the full version of my posts.
-			</p>
+		<div className="prose prose-invert max-w-none m-10">
+			<div>
+				<h1>A developer who provides a banner to the world.</h1>
+			</div>
+			<div
+				className="flex flex-row
+			">
+				<div className="mr-10">
+					<Image
+						src={"/imgs/profile.jpg"}
+						width={200}
+						height={0}
+						alt="profile"
+					/>
+				</div>
+				<div>
+					<h2>Jeseok Lee</h2>
+					<p>
+						Hello there 👋
+						<br />
+						I&rsquo;m <b>Jeseok Lee</b> from south Korea
+					</p>
+					2006. 08. 03 / 이제석 <br />
+					realjaylee83@gmail.com
+					<br />
+					Gwangju-si, Gyeonggi, South Korea
+				</div>
+			</div>
 		</div>
 	);
 }
